ed0575316e77488ac86158867971e82d32d274a8
CaloP <arkaprexxd@gmail.com>
2024-08-03 12:19:59 -0500

    nuevo

---
+++ unidad_01/html/javacrip/basic/04_taller/js/script.js
@@ -0,0 +1,13 @@
+function seleccionar_opcion (opcion){
+    var opcion_1 = document.getElementById("opcion_01")
+    var opcion_1 = document.getElementById("opcion_02")
+
+    if (opcion == 1){
+        opcion_1.style.zIndex = 2
+        opcion_1.style.zIndex = 1
+
+    } else if (opcion == 2){
+        opcion_1.style.zIndex = 1
+        opcion_1.style.zIndex = 2
+    }
+}
